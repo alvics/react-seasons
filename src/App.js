441173ac68cf2d894e1bb7cade9509b11345e8a5
alvics <alvics71@hotmail.com>
2020-05-06 12:16:04 +1000

    seasons app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import SeasonsDisplay from './SeasonsDisplay';
+import Spinner from './Spinner';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    lat: null,
+    lon: null,
+    errorMessage: ''
+  };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) =>
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude
+        }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // Helper method
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonsDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner message="Please except location request" />;
+  }
+
+  render() {
+    return <div className="border-red">{this.renderContent()}</div>;
+  }
 }
 
 export default App;
